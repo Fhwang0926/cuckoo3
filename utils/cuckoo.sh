210dca6cd373172a599cb403f1b552cc1fba879a
Fhwang <donghwa.dev@gmail.com>
2024-05-12 21:23:33 +0900

    test

--- utils/cuckoo.sh
+++ utils/cuckoo.sh
@@ -5,7 +5,7 @@
 start() {
   stop
   # nohup bash -c /opt/cuckoo3.1/utils/cuckoo.service
-  bash -c "/opt/cuckoo3.1/utils/cuckoo.service 2>&1 &"
+  bash -c "sudo -u cuckoo /opt/cuckoo3.1/utils/cuckoo.service 2>&1 &"
   echo $! > /tmp/cuckoo.pid && cat /tmp/cuckoo.pid
 }
 
